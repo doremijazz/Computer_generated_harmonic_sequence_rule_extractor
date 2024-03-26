312b514580ae847190a5effa95ee3e07a42d566b
doremijazz <55767732+doremijazz@users.noreply.github.com>
2024-03-26 22:47:16 +0100

    Add files via upload

---
+++ Oracle/vmo_master/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vmo",
+    version="0.30.5",
+    author="Cheng-i Wang",
+    author_email="chw160@ucsd.edu",
+    description="Variable Markov Oracle in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wangsix/vmo",
+    project_urls={
+        "Bug Tracker": "https://github.com/wangsix/vmo/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=['vmo', 'vmo.VMO', 'vmo.analysis', 'vmo.realtime', 'vmo.VMO.utility', 'vmo.VMO.utility.distances'],
+    python_requires=">=3.7",
+)
